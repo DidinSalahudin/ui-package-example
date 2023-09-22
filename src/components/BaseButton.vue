fe37da7ab8aeee73c30c1a1ca8fa1b51f636ce7f
DidinSalahudin <didinsalahudin@gmail.com>
2023-09-22 10:01:18 +0700

    first commit

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button id="baseButton">
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+    import { computed } from 'vue';
+
+    type ButtonProps = {
+        size?: number,
+        color?: string
+    }
+
+    const props = withDefaults(defineProps<ButtonProps>(), {
+        size: 16,
+        color: "skyblue",
+    });
+
+    const fontSize = computed(() => `${props.size}px`);
+</script>
+
+<style scoped>
+    #baseButton {
+        padding: 1rem 2rem;
+        cursor: pointer;
+        border: none;
+        background-color: v-bind(color);
+        font-size: v-bind(fontSize);
+    }
+</style>
